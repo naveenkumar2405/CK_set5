723ef2678381163950cf6a9d589331cc9240452a
Naveen_Kumar24 <51779899+naveenkumar2405@users.noreply.github.com>
2019-08-06 20:32:38 +0530

    Create CK41.cpp

---
+++ CK41.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+  string str;
+  int n;
+
+  cin >> str >> n;
+
+  while(n-- != 0)
+    cout << str << "\n";
+
+  return 0;
+}
