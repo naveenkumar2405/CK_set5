849c25b754e71fbe7bb5959f2672969cd95f5c09
Naveen_Kumar24 <51779899+naveenkumar2405@users.noreply.github.com>
2019-08-06 20:44:32 +0530

    Create CK43.cpp

---
+++ CK43.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+  string str1, str2;
+
+  cin >> str1 >> str2;
+
+  str1.append(str2);
+
+  cout << str1;
+
+  return 0;
+}
